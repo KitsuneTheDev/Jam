99c34c4dadfb258c848c9e1e7e12f04cddf1c6c5
KitsuneTheDev <163762953+KitsuneTheDev@users.noreply.github.com>
2025-09-22 18:56:03 +0300

    initial

---
+++ backend/data/users.js
@@ -0,0 +1,31 @@
+// users.js
+
+const users = [
+    {
+        id: 1,
+        username: 'admin',
+        email: 'kitsunethedeveloper.com',
+        role: 'admin',
+        purchasedTickets: [
+            // Tickets that are bought by user
+        ],
+    }, {
+        id: 2,
+        username: 'testUser_1',
+        email: 'celikkolozan@gmail.com',
+        role: 'user',
+        purchasedTickets: [
+            // Tickets that are bought by user
+        ],
+    }, {
+        id: 3,
+        username: 'testUser_2',
+        email: 'ozeebuzee@gmail.com',
+        roler: 'user',
+        purchasedTickets: [
+            // Tickets that are bought by user
+        ]
+    }
+];
+
+export { users };
