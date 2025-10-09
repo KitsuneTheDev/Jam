65352fb6f5c8ab6e4f6e175f00f4ccae7237dd32
KitsuneTheDev <163762953+KitsuneTheDev@users.noreply.github.com>
2025-10-10 00:21:42 +0300

    update
    -main look finished

--- frontend/src/components/tickets/Tickets.jsx
+++ frontend/src/components/tickets/Tickets.jsx
@@ -1,9 +1,26 @@
 import styles from './Tickets.module.css';
+import { userTickets } from '../../data/userTickets';
 
 export default function Tickets() {
+
+    const demoTicket = userTickets[0];
+
     return(
         <div className={styles.ticketsContainer}>
-            <h1>TICKETS</h1>
+            <div className={styles.ticketsInner}>
+                <div className={styles.ticketContent}>
+                    <div className={styles.contentBody}>
+                        <div className={styles.firstContent}>KEEP</div>
+                        <div className={styles.secondContent}>THIS</div>
+                        <div className={styles.thirdContent}>COUPON</div>
+                    </div>
+                    <div className={styles.contentHeader}>{demoTicket.userTicketId}</div>
+                </div>
+            </div>
+            <div className={styles.ticketCornerHole}></div>
+            <div className={styles.ticketCornerHole}></div>
+            <div className={styles.ticketCornerHole}></div>
+            <div className={styles.ticketCornerHole}></div>
         </div>
     );
 }
